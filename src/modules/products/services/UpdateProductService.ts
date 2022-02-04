fc586f2ec1db42461baba955d3c661ee08d05535
Bruno Lopes <brunofabricio14@hotmail.com>
2022-02-03 22:53:46 -0300

    Inserindo service para update de product

---
+++ src/modules/products/services/UpdateProductService.ts
@@ -0,0 +1,41 @@
+import { AppError } from '@shared/errors/App.Error';
+import { getCustomRepository } from 'typeorm';
+import { ProductRepository } from '../typeorm/repositories/ProductsRepository';
+
+interface IRequest {
+  id: string;
+  name: string;
+  price: number;
+  quantity: number;
+}
+
+class UpdateProductService {
+  public async execute({ id, name, price, quantity }: IRequest) {
+    const productRepository = getCustomRepository(ProductRepository);
+    const product = await productRepository.findOne({
+      where: id,
+    });
+
+    if (!product) {
+      throw new AppError('Product not found.');
+    }
+
+    const productExists = await productRepository.findByName(name);
+
+    if (productExists && name !== product.name) {
+      throw new AppError('This product already exists');
+    }
+
+    Object.assign(product, {
+      name,
+      price,
+      quantity,
+    });
+
+    await productRepository.save(product);
+
+    return product;
+  }
+}
+
+export { UpdateProductService };
